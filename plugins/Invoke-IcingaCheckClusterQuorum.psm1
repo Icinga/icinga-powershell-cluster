3a16f38fd412bad1a3063b5b53d66c40044d6f69
Yonas Habteab <Yonas.Habteab@netways.de>
2020-10-10 15:56:33 +0200

    Implement icinga check cluster quorum plugin

---
+++ plugins/Invoke-IcingaCheckClusterQuorum.psm1
@@ -0,0 +1,12 @@
+<#
+#>
+function Invoke-IcignaCheckClusterQuorum()
+{
+    param(
+        [switch]$NoPerfData = $FALSE,
+        [ValidateSet(0, 1, 2)]
+        $Verbosity          = 0
+    );
+
+    
+}
